c35700829eab6c27f5788a049a33126958011bde
Eric Ghoubiguian <ericghoubiguian@live.fr>
2026-04-01 15:38:24 +0200

    new function

--- main.py
+++ main.py
@@ -8,7 +8,23 @@ def get_all_timezones():
     return pytz.all_timezones
 	
 #
+def get_datetime_for_particular_timezone(destination_timezone):
+
+    #
+    if destination_timezone in pytz.all_timezones :
+    
+         #
+         return 0
+    
+    #
+    else :
+    
+         #
+         return -1
+    
+#
 if __name__ == "__main__":
 
     #
     print(get_all_timezones())
+    print(get_datetime_for_particular_timezone("Europe/Berlin"))
