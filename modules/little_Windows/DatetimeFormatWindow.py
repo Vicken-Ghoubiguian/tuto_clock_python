90773a02dc79c1e4550fe0a8748c115414341a75
Eric Ghoubiguian <ericghoubiguian@live.fr>
2026-04-20 10:53:16 +0200

    Adding new datetime formats and window size

--- modules/little_Windows/DatetimeFormatWindow.py
+++ modules/little_Windows/DatetimeFormatWindow.py
@@ -22,7 +22,7 @@ class DateTimeFormatWindow(tkinter.Tk) :
           self.resizable(False, False)
 
           #
-          self.geometry('500x500')
+          self.geometry('500x400')
 
           #
           self.datetime_format_label = tkinter.Label(self, font=('calibri', 10, 'bold'))
@@ -35,30 +35,26 @@ class DateTimeFormatWindow(tkinter.Tk) :
                                  "%A : full weekday name.\n" \
                                  "%w : weekday as a decimal number.\n" \
                                  "%d : day of the month as a zero added decimal.\n" \
-                                 "%-d : day of the month as a decimal number.\n" \
                                  "%b : abbreviated month name.\n" \
                                  "%B : full month name.\n" \
                                  "%m : month as a zero added decimal number.\n" \
-                                 "%-m : month as a decimal number.\n" \
                                  "%y : year without century as a zero added decimal number.\n" \
-                                 "%-y : year without century as a decimal number.\n" \
                                  "%Y : year with century as a decimal number.\n" \
                                  "%H : hour (24-hour clock) as a zero added decimal number.\n" \
-                                 "%-H : hour (24-hour clock) as a decimal number.\n" \
                                  "%I : hour (12-hour clock) as a zero added decimal number.\n" \
-                                 "%-I : hour (12-hour clock) as a decimal number.\n" \
                                  "%p : locale’s AM or PM.\n" \
                                  "%M : minute as a zero added decimal number.\n" \
-                                 "%-M : minute as a decimal number.\n" \
                                  "%S : second as a zero added decimal number.\n" \
-                                 "%-S : second as a decimal number.\n" \
                                  "%f : microsecond as a decimal number, zero added on the left.\n" \
                                  "%z : UTC offset in the form +HHMM or -HHMM.\n" \
                                  "%Z : time zone name.\n" \
                                  "%j : day of the year as a zero added decimal number.\n" \
-                                 "%-j : day of the year as a decimal number.\n" \
                                  "%U : week number of the year (Sunday as the first day of the week).\n" \
-                                 "%W : week number of the year (Monday as the first day of the week)."
+                                 "%W : week number of the year (Monday as the first day of the week).\n" \
+                                 "%c : locale’s appropriate date and time representation.\n" \
+                                 "%x : locale’s appropriate date representation.\n" \
+                                 "%X : locale’s appropriate time representation.\n" \
+                                 "%% : A literal '%' character."
 
           #
           self.datetime_format_label.config(text=datetime_format_text)
