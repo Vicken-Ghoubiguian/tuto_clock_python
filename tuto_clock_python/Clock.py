ded3a29c3649026c3381ed36f79d82a170ec5b22
Eric Ghoubiguian <ericghoubiguian@live.fr>
2026-05-15 16:53:20 +0200

    Clock from tkinter to pyside

--- tuto_clock_python/Clock.py
+++ tuto_clock_python/Clock.py
@@ -31,379 +31,177 @@ except ImportError:
      from . import OpenedWindowException as OpenedWindowException
 
 # from the needed Python modules import needed components
-import tkinter
-import tkinter.ttk
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QLineEdit, QMenuBar, QMenu
+from PySide6.QtGui import QIcon, QFont
+from PySide6.QtCore import QTimer
+
 import argparse
 import os
-import platform
+import sys
 
-from tzlocal.windows_tz import win_tz
-from PIL import Image, ImageTk
+class Clock(QApplication):
 
-# Definition of the 'Clock' class
-class Clock(tkinter.Tk) :
+    def __init__(self, timezone, dt_format, zoom_map, title, width, height):
 
-     # Definition of the 'Clock' class constructor
-     def __init__(self, timezone, dt_format, zoom_map, title, width, height):
-          
-          # Definition of the main GUI
-          tkinter.Tk.__init__(self)
+        super().__init__(sys.argv)
 
-          # Definition of the main GUI title
-          self.title(title)
+        self.timezone = timezone
+        self.datetime_format = dt_format
+        self.initial_datetime_format = dt_format
 
-          # Instruction that disables the ‘X’ (close) button in the top-right corner of the window.
-          self.protocol('WM_DELETE_WINDOW', 'break')
+        self.littleWindow = False
+        self.littleWindowType = False
 
-          #
-          self.gui_image = ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "clock.png")))
+        # MAIN WINDOW
+        self.window = QMainWindow()
+        self.window.setWindowTitle(title)
+        self.window.setFixedSize(int(width), int(height))
+        self.window.setWindowIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "clock.png")))
 
-          # Implementation of the main GUI image
-          self.iconphoto(True, self.gui_image)
+        # COMBOBOX timezone
+        self.tz_comboBox = QComboBox(self.window)
+        self.tz_comboBox.setGeometry(10, 10, 1480, 25)
 
-          # Definition of the window dimensions
-          self.geometry("x".join([str(width), str(height)]))
+        self.tz_comboBox.addItems(dt_management.get_all_timezones())
+        self.tz_comboBox.setCurrentText(self.timezone)
+        self.tz_comboBox.currentTextChanged.connect(self.get_Selected_Timezone)
 
-          # Set the window not resizable
-          self.resizable(False, False)
+        # ENTRY datetime format
+        self.dt_format_entry = QLineEdit(self.window)
+        self.dt_format_entry.setGeometry(10, 45, 1480, 25)
+        self.dt_format_entry.setText(self.datetime_format)
+        self.dt_format_entry.textChanged.connect(self.get_Selected_Datetime_format)
 
-          # Set the current timezone the same as filled in argument
-          self.timezone = timezone
+        # LABEL clock
+        self.clock_label = QLabel(self.window)
+        self.clock_label.setGeometry(10, 100, 1480, 200)
+        self.clock_label.setFont(QFont("Calibri", 40, QFont.Bold))
 
-          # Set the initial datetime format
-          self.initial_datetime_format = dt_format
+        # MENU
+        menubar = QMenuBar(self.window)
+        menu = QMenu("Menu", self.window)
 
-          # Set the current datetime format as the initial one
-          self.datetime_format = self.initial_datetime_format
+        menu.addAction("Cartography", self.openCartographyWindow)
+        menu.addAction("Weather", self.openWeatherWindow)
+        menu.addAction("Datetime formats", self.openDatetimeFormatWindow)
+        menu.addAction("User guide", self.openUserGuideWindow)
+        menu.addAction("Exit", self.window.close)
 
-          # There is no window which is opened (class attribute 'littleWindow' as False)
-          self.littleWindow = False
+        menubar.addMenu(menu)
+        #self.window.setMenuBar(menubar)
 
-          # 
-          self.littleWindowType = False
+        # TIMER (remplace after())
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.clock_time)
+        self.timer.start(1000)
 
-          # Get all available timezones into the 'all_timezones' variable
-          all_timezones = dt_management.get_all_timezones()
+        self.clock_time()
 
-          # Definition of the 'tz_comboBox' combobox which is the combobox of all timezones
-          self.tz_comboBox = tkinter.ttk.Combobox(self, width = 110, values = all_timezones, state = "readonly")
+    # OPEN WINDOWS
+    def openCartographyWindow(self):
 
-          #
-          self.tz_comboBox.set(self.timezone)
+        if not self.littleWindowType:
 
-          #
-          self.tz_comboBox.bind('<<ComboboxSelected>>', self.get_Selected_Timezone)
+            self.littleWindowType = "webview"
 
-          #
-          self.tz_comboBox.pack()
+            self.littleWindow = CartographyWindow.CartographyWindow(
+                self.timezone, "World's map", 5, 1200, 800,
+                "generated_map_clock.html"
+            )
 
-          #
-          self.dt_format_entry = tkinter.Entry(self, width = 113)
+            self.littleWindow.start()
 
-          #
-          self.dt_format_entry.insert(0, self.datetime_format)
+        else:
+            raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
 
-          # Bind the Entry to key release events (user typing)
-          self.dt_format_entry.bind('<KeyRelease>', self.get_Selected_Datetime_format)
+    def openWeatherWindow(self):
 
-          #
-          self.dt_format_entry.pack()
+        if not self.littleWindowType:
 
-          #
-          self.clock_label = tkinter.Label(self, font=('calibri', 40, 'bold'), background='blue', foreground='yellow')
+            self.littleWindowType = "qt"
+            self.littleWindow = WeatherWindow.WeatherWindow()
+            self.littleWindow.show()
 
-          #
-          self.clock_label.pack(anchor="center")
+        else:
+            raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
 
-          #
-          self.clock_time()
+    def openDatetimeFormatWindow(self):
 
-          # Definition of the 'menubar' menubar
-          menubar = tkinter.Menu(self)
+        if not self.littleWindowType:
 
-          # Integration of the menubar in the GUI
-          self.config(menu = menubar)
+            self.littleWindowType = "qt"
+            self.littleWindow = DatetimeFormatWindow.DateTimeFormatWindow(500, 500, "Datetime format")
+            self.littleWindow.show()
 
-          #
-          timezone_menu = tkinter.Menu(menubar)
+        else:
+            raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
 
-          # Definition of the 'cartography' menu command to display the current timezone location on a map
-          timezone_menu.add_command(
-               label="Cartography",
-               command=self.destroy
-          )
+    def openUserGuideWindow(self):
 
-          # Definition of the 'Weather' menu command to display weather of the current timezone location
-          timezone_menu.add_command(
-               label="Weather",
-               command=self.destroy
-          )
+        if not self.littleWindowType:
 
-          # Definition of the 'Datetime formats' menu command to inform the user about the datetime formats that could be use in the application
-          timezone_menu.add_command(
-               label="Datetime formats",
-               command=self.destroy
-          )
+            self.littleWindowType = "qt"
+            self.littleWindow = UserGuideWindow.UserGuideWindow()
+            self.littleWindow.show()
 
-          # Definition of the 'User guide' menu command to guide the user on the application features
-          timezone_menu.add_command(
-               label="User guide",
-               command=self.destroy
-          )
+        else:
+            raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
 
-          # Definition of the 'Exit' menu command to exit the application
-          timezone_menu.add_command(
-               label="Exit",
-               command=self.destroy
-          )
+    def closeWindow(self):
 
-          # Definition of the 'menu' menu
-          menubar.add_cascade(
-               label="menu",
-               menu=timezone_menu
-          )
+        self.littleWindowType = False
+        self.littleWindow = False
 
-     #
-     def openCartographyWindow(self) :
+    # EVENTS
+    def get_Selected_Datetime_format(self):
+        self.datetime_format = self.dt_format_entry.text()
 
-          #
-          if self.littleWindowType == False :
+    def get_Selected_Timezone(self):
+        self.timezone = self.tz_comboBox.currentText()
 
-               #
-               self.littleWindowType = "webview"
+    # CLOCK UPDATE
+    def clock_time(self):
 
-               #
-               self.littleWindow = CartographyWindow.CartographyWindow(self.timezone, "World's map", 5, 1200, 800, "generated_map_clock.html")
+        dt = dt_management.get_datetime_for_particular_timezone(self.timezone)
 
-               #
-               self.littleWindow.window.events.closed += self.closeWindow
+        if dt is None:
+            return
 
-               #
-               self.littleWindow.start()
-          
-          #
-          else :
+        self.clock_label.setText(
+            f"{self.timezone} : {dt.strftime(self.datetime_format)}"
+        )
 
-               #
-               raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
+    def run(self):
 
-     #
-     def openWeatherWindow(self) :
+        self.window.show()
+        sys.exit(self.exec())
 
-          #
-          if self.littleWindowType == False :
 
-               #
-               self.littleWindowType = "tkinter"
+# MAIN
+if __name__ == "__main__":
 
-               #
-               self.littleWindow = WeatherWindow.WeatherWindow()
-
-               #
-               self.littleWindow.wm_protocol("WM_DELETE_WINDOW", self.closeWindow)
-
-               #
-               self.littleWindow.mainloop()
-
-          #
-          else :
-
-               #
-               raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
-
-     #
-     def openDatetimeFormatWindow(self) :
-
-          #
-          if self.littleWindowType == False :
-
-               #
-               self.littleWindowType = "tkinter"
-
-               # Definition of the 'DateTimeFormatWindow' window to inform the user about the different available datetime formats
-               self.littleWindow = DatetimeFormatWindow.DateTimeFormatWindow(500, 500, "Datetime format")
-
-               #
-               self.littleWindow.wm_protocol("WM_DELETE_WINDOW", self.closeWindow)
-
-               #
-               self.littleWindow.mainloop()
-
-          #
-          else :
-
-               #
-               raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
-
-     #
-     def openUserGuideWindow(self) :
-
-          #
-          if self.littleWindowType == False :
-
-               #
-               self.littleWindowType = "tkinter"
-
-               # Definition of the 'UserGuideWindow' window to inform the user about the clock features
-               self.littleWindow = UserGuideWindow.UserGuideWindow()
-
-               #
-               self.littleWindow.wm_protocol("WM_DELETE_WINDOW", self.closeWindow)
-
-               #
-               self.littleWindow.mainloop()
-
-          #
-          else :
-
-               #
-               raise OpenedWindowException.OpenedWindowException("Window already opened !", 400)
-
-     #
-     def closeWindow(self) :
-
-          #
-          if self.littleWindowType == "tkinter" :
-
-               #
-               self.littleWindow.destroy()
-
-          #
-          self.littleWindowType = False
-
-          #
-          self.littleWindow = False
-
-     #
-     def get_Selected_Datetime_format(self, eventObject) :
-
-          #
-          self.datetime_format = self.dt_format_entry.get()
-
-          #
-          self.clock_time()
-
-     #
-     def get_Selected_Timezone(self, eventObject) :
-
-          #
-          self.timezone = self.tz_comboBox.get()
-
-          #
-          self.clock_time()
-
-     # Definition of the 'clock_time' function which displays the current datetime of the current timezone
-     def clock_time(self) :
-
-          # In the case where there is a country code, so...
-          if dt_management.get_countrycode_of_timezone(self.timezone) != None :
-               
-               #
-               try :
-
-                    # ...you get all datas from the particular country code
-                    country = dt_management.get_datas_from_particular_countrycode(dt_management.get_countrycode_of_timezone(self.timezone))
-
-                    #
-                    unicode_country = country["flag"].encode('utf-8')
-
-                    # Check if the platform is Windows and if the country name is "RÃ©union"
-                    if platform.system() == "Windows" and country["name"] == "RÃ©union" :
-
-                         #
-                         self.clock_label.config(text=dt_management.get_datetime_for_particular_timezone(self.timezone).strftime("".join([self.timezone," (",unicode_country.decode('unicode_escape')," ",country["name"].encode("latin1").decode("utf-8"),") : ",self.datetime_format])))
-
-                    #
-                    elif platform.system() == "Windows" and country["name"] == "Ã...land Islands" :
-
-                         #
-                         self.clock_label.config(text=dt_management.get_datetime_for_particular_timezone(self.timezone).strftime("".join([self.timezone," (",unicode_country.decode('unicode_escape')," ",country["name"].decode("utf-8"),") : ",self.datetime_format])))
-
-                    #
-                    else :
-
-                         #
-                         self.clock_label.config(text=dt_management.get_datetime_for_particular_timezone(self.timezone).strftime("".join([self.timezone," (",unicode_country.decode('unicode_escape')," ",country["name"],") : ",self.datetime_format])))
-
-               #
-               except :
-
-                    #
-                    self.datetime_format = self.initial_datetime_format
-
-                    #
-                    country = dt_management.get_datas_from_particular_countrycode(dt_management.get_countrycode_of_timezone(self.timezone))
-
-                    #
-                    unicode_country = country["flag"].encode('utf-8')
-
-                    #
-                    if platform.system() == "Windows" and country["name"] == "RÃ©union" :
-
-                         #
-                         self.clock_label.config(text=dt_management.get_datetime_for_particular_timezone(self.timezone).strftime("".join([self.timezone," (",unicode_country.decode('unicode_escape')," ",country["name"].encode("latin1").decode("utf-8"),") : ",self.datetime_format])))
-                    #
-                    elif platform.system() == "Windows" and country["name"] == "Ã...land Islands" :
-
-                         #
-                         self.clock_label.config(text=dt_management.get_datetime_for_particular_timezone(self.timezone).strftime("".join([self.timezone," (",unicode_country.decode('unicode_escape')," ",country["name"].decode("utf-8"),") : ",self.datetime_format])))
-
-                    #
-                    else :
-
-                         #
-                         self.clock_label.config(text=dt_management.get_datetime_for_particular_timezone(self.timezone).strftime("".join([self.timezone," (",unicode_country.decode('unicode_escape')," ",country["name"],") : ",self.datetime_format])))
-
-          #
-          else :
-
-               #
-               self.clock_label.config(text=dt_management.get_datetime_for_particular_timezone(self.timezone).strftime("".join([self.timezone," : ",self.datetime_format])))
-
-          #
-          self.clock_label.after(1000, self.clock_time)
-
-#
-if __name__ == "__main__" :
-    
-    # Get the current timezone of the system
     iana_tz = dt_management.return_iana_timezone()
 
-    #
-    parser = argparse.ArgumentParser(description="Simple app with timezones")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--timezone', default=iana_tz)
+    parser.add_argument('--dt_format', default='%Y-%m-%d %H:%M:%S')
+    parser.add_argument('--zoom_map', default=5)
+    parser.add_argument('--width', default=1500)
+    parser.add_argument('--height', default=500)
+    parser.add_argument('--title', default='Clock')
 
-    #
-    parser.add_argument('--timezone', action="store", dest='timezone', default=iana_tz)
-
-    #
-    parser.add_argument('--dt_format', action="store", dest='dt_format', default='%Y-%m-%d %H:%M:%S')
-
-    #
-    parser.add_argument('--zoom_map', action="store", dest='zoom_map', default=5)
-
-    #
-    parser.add_argument('--width', action="store", dest='width', default=1500)
-
-    #
-    parser.add_argument('--height', action="store", dest='height', default=500)
-
-    #
-    parser.add_argument('--title', action="store", dest='title', default='Clock')
-    
-    #
     args = parser.parse_args()
 
-    #
-    if dt_management.get_datetime_for_particular_timezone(args.timezone) != None :
+    if dt_management.get_datetime_for_particular_timezone(args.timezone) is None:
+        raise TimeZoneException.TimeZoneException("Timezone unknown !", 400)
 
-         #
-         clock = Clock(args.timezone, args.dt_format, args.zoom_map, args.title, args.width, args.height)
+    app = Clock(
+        args.timezone,
+        args.dt_format,
+        args.zoom_map,
+        args.title,
+        args.width,
+        args.height
+    )
 
-    #
-    else :
-         
-         #
-         raise TimeZoneException.TimeZoneException("Timezone unknown !", 400)
-
-    #
-    clock.mainloop()
+    app.run()
