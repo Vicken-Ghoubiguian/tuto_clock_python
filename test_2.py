32c1a4719630b8e6e4c9574ad223fa0b00f89aeb
Eric Ghoubiguian <ericghoubiguian@live.fr>
2026-05-20 18:33:36 +0200

    Adding some new arguments to input

--- test_2.py
+++ test_2.py
@@ -13,7 +13,19 @@ from PySide6.QtWidgets import QApplication
 timezone = input("Entrez une timezone (ex: Europe/Paris, America/New_York) : ")
 
 #
+title = input("Entrez le titre que vous voulez pour afficher la fenêtre (ex: 'World's map') : ")
+
+#
 dt_format = input("Entrez un format de temps et de date (ex: %Y-%m-%d %H:%M:%S) : ")
+
+#
+zoom_map = input("Entrez la valeur du zoom sur la carte (ex: 5) : ")
+
+#
+width = input("Entrez la largeur de la fenêtre (ex: 1200) : ")
+
+#
+height = input("Entrez la hauteur de la fenêtre (ex: 800) : ")
 
 #
 if dt_management.get_datetime_for_particular_timezone(timezone) != None :
@@ -51,7 +63,7 @@ if gc is not None :
     app = QApplication(sys.argv)
     
     #
-    viewMap = CartographyWindow.CartographyWindow(timezone, "World's map", 5, 1200, 800, "generated_map_test_2.html")
+    viewMap = CartographyWindow.CartographyWindow(timezone, title, int(zoom_map), int(width), int(height), "generated_map_test_2.html")
          
     #
     viewMap.start()
